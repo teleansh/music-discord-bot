4a46a39f32e8361d765faaeb8e00d4fb6bf68bb9
darahask <darahask12@gmail.com>
2022-01-19 14:55:52 +0530

    token issue rectification

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const { Client } = require('discord.js');
 const { joinVoiceChannel,
     createAudioPlayer,
     AudioPlayerStatus } = require('@discordjs/voice')
+const play = require('play-dl')
 const createUrlList = require('./utils/audio-list')
 const createResource = require('./utils/create-resource')
 const {getHelpEmbed, getListEmbed} = require('./utils/message-embed')
+
+play.setToken({
+    spotify: {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: process.env.REFRESH_TOKEN,
+        market: 'IN'
+    }
+})
 
 let playerObj = new Map();
 let resourceQueue = new Map();
